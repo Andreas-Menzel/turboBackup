66745c9f41496fa53bd2fa6abee47d6cffde3f7c
unknown <ygh@mail.de>
2022-02-21 11:22:41 +0100

    Changed to use Windows directory links

--- turboBackup.py
+++ turboBackup.py
@@ -1,42 +1,33 @@
 #!/usr/bin/env python3
 
-#from constants import *
-import sys
 import os
 import datetime
 
-
-def main():
-    sourceDir= "testOrdner" + "/"
-    backupDir= "backupOrdner"
+def doBackup(sourceDir, backupDir):
     datetimeString= (datetime.datetime.now()).strftime("%Y-%m-%d_%H-%M-%S")
 
     if not os.path.exists(backupDir + "/" + datetimeString + "_new"):
-        os.makedirs(backupDir + "/" + datetimeString + "_new")
+        os.makedirs(backupDir + "/" + datetimeString)
 
     latestBackup= "placeholder"
-    for i in os.listdir(path=backupDir):
-        if(i.endswith("latest")):
-                latestBackup= i
+    if os.path.exists(backupDir + "/" + "latestBackup"):
+        latestBackup= "latestBackup"
 
     os.system(
         "rsync -av " + 
         "--link-dest " + "..\\" + latestBackup + " " +
         sourceDir + " " +
-        backupDir + "\\" + datetimeString + "_new"
+        backupDir + "\\" + datetimeString
     )
 
-    if os.path.exists(backupDir + "/" + latestBackup):
-        os.rename(
-            (backupDir + "/" + latestBackup), 
-            (backupDir + "/" + latestBackup[0:19])
-        )
+    os.system("rmdir " + backupDir + "\\" + "latestBackup")
+    os.system("mklink /J " + backupDir + "\\" + "latestBackup " +
+        backupDir + "\\" + datetimeString)
 
-    os.rename(
-        (backupDir + "/" + datetimeString + "_new"), 
-        (backupDir + "/" + datetimeString + "_latest")
-    )
-
+def main():
+    sourceDir= "testOrdner" + "/"
+    backupDir= "backupOrdner"
+    doBackup(sourceDir, backupDir)
 
 if __name__ == "__main__":
     main()
