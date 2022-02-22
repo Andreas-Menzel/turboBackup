a4f66b379ae9dfba2b24677d0d32aca1abd969bd
unknown <ygh@mail.de>
2022-02-22 16:14:05 +0100

    resolved more path issues, now supports absolute and relative paths, rsync now copys the directory contents instead of the directory itself

--- turboBackup.py
+++ turboBackup.py
@@ -7,7 +7,7 @@ import os
 import shutil
 import datetime
 import platform
-from pathlib import Path, PureWindowsPath
+from pathlib import Path, PurePosixPath
 
 def deleteBackups(backupDir, pattern, keep):
     directoryList= os.listdir(backupDir)
@@ -37,38 +37,41 @@ def doBackup(sourceDir, backupDir, name="", keep=0):
     if not os.path.exists(backupDir):
         os.makedirs(backupDir)
 
-    latestBackupDir= "placeholder"
+    latestBackup= "placeholder"
     directoryList= os.listdir(backupDir)
     if directoryList:
-        latestBackupDir= Path(max(directoryList))
+        latestBackup= max(directoryList)
 
     if platform.system() == "Windows":
+        sourceDirTmp= str(PurePosixPath(sourceDir))
+        backupDirTmp= str(PurePosixPath(backupDir))
+        sourceDirTmp= "/cygdrive/" + sourceDirTmp[0] + sourceDirTmp[3:]
+        backupDirTmp= "/cygdrive/" + backupDirTmp[0] + backupDirTmp[3:]
+        
+        latestBackupTmp= backupDirTmp + "/" + latestBackup
+         
         os.system(
             "rsync -a " +
             "--delete " +
-            "--link-dest " + ".\\..\\" + str(latestBackupDir) + " " +
-            ".\\" + str(sourceDir) + " " +
-            ".\\" + str(backupDir) + "\\" + datetimeString + name
+            "--link-dest '" + latestBackupTmp + "' " +
+            "'" + sourceDirTmp + "/' " +
+            "'" + backupDirTmp + "/" + datetimeString + name + "'"
         )
     else:
         os.system(
             "rsync -a " +
             "--delete " +
-            "--link-dest " + "./../" + str(latestBackupDir) + " " +
-            "./" + str(sourceDir) + " " +
-            "./" + str(backupDir) + "/" + datetimeString + name
+            "--link-dest '" + str(backupDir) + "/" + latestBackup + "' " +
+            "'" + str(sourceDir) + "/' " +
+            "'" + str(backupDir) + "/" + datetimeString + name + "'"
         )
 
     if keep > 0:
         deleteBackups(backupDir, name, keep)
 
 def pathResolveWrapper(sourceDir, backupDir, name="", keep=0):
-    if platform.system() == "Windows":
-        sourceDir= PureWindowsPath(sourceDir)
-        backupDir= PureWindowsPath(backupDir)
-    else:
-        sourceDir= Path(sourceDir)
-        backupDir= Path(backupDir)
+    sourceDir= Path(sourceDir).absolute()
+    backupDir= Path(backupDir).absolute()
     
     doBackup(sourceDir, backupDir, name=name, keep=keep)
 
