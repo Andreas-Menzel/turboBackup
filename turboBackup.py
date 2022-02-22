8806f05473d29d5b3fc75374346bc4ed1a8771c4
unknown <ygh@mail.de>
2022-02-22 11:46:45 +0100

    resolved some path issues

--- turboBackup.py
+++ turboBackup.py
@@ -7,65 +7,75 @@ import os
 import shutil
 import datetime
 import platform
+from pathlib import Path, PureWindowsPath
 
 def deleteBackups(backupDir, pattern, keep):
     directoryList= os.listdir(backupDir)
-    if(not directoryList):
+    if not directoryList:
         return
     
     patternFiltered=[]
     for i in directoryList:
-        if(i.endswith(pattern)):
+        if i.endswith(pattern):
             patternFiltered.append(i)
 
-    if(len(patternFiltered) <= keep):
+    if len(patternFiltered) <= keep:
         return
     
     patternFiltered.sort()
     toDelete= patternFiltered[0:len(patternFiltered)-keep]
 
     for i in toDelete:
-        shutil.rmtree(backupDir + "/" + i)    
+        shutil.rmtree(backupDir / Path(i))    
 
 
 def doBackup(sourceDir, backupDir, name="", keep=0):
     datetimeString= (datetime.datetime.now()).strftime("%Y-%m-%d_%H-%M-%S")
-    if(name != ""):
+    if name != "":
         name= "_" + name
 
     if not os.path.exists(backupDir):
         os.makedirs(backupDir)
 
-    latestBackup= "placeholder"
+    latestBackupDir= "placeholder"
     directoryList= os.listdir(backupDir)
-    if(directoryList):
-        latestBackup= max(directoryList)        
+    if directoryList:
+        latestBackupDir= Path(max(directoryList))
 
-    if(platform.system() == "Windows"):
+    if platform.system() == "Windows":
         os.system(
             "rsync -a " +
             "--delete " +
-            "--link-dest " + ".\\..\\" + latestBackup + " " +
-            ".\\" + sourceDir + " " +
-            ".\\" + backupDir + "\\" + datetimeString + name
+            "--link-dest " + ".\\..\\" + str(latestBackupDir) + " " +
+            ".\\" + str(sourceDir) + " " +
+            ".\\" + str(backupDir) + "\\" + datetimeString + name
         )
-    
-    if((platform.system() == "Linux") or (platform.system() == "Darwin")):
+    else:
         os.system(
-            "rsync -av " +
+            "rsync -a " +
             "--delete " +
-            "--link-dest " + "./../" + latestBackup + " " +
-            "./" + sourceDir + " " +
-            "./" + backupDir + "/" + datetimeString + name
+            "--link-dest " + "./../" + str(latestBackupDir) + " " +
+            "./" + str(sourceDir) + " " +
+            "./" + str(backupDir) + "/" + datetimeString + name
         )
 
-    if(keep > 0):
+    if keep > 0:
         deleteBackups(backupDir, name, keep)
+
+def pathResolveWrapper(sourceDir, backupDir, name="", keep=0):
+    if platform.system() == "Windows":
+        sourceDir= PureWindowsPath(sourceDir)
+        backupDir= PureWindowsPath(backupDir)
+    else:
+        sourceDir= Path(sourceDir)
+        backupDir= Path(backupDir)
+    
+    doBackup(sourceDir, backupDir, name=name, keep=keep)
 
 def main():
     sourceDir= "testOrdner"
     backupDir= "backupOrdner"
-    doBackup(sourceDir, backupDir, name="test", keep=5)
+    pathResolveWrapper(sourceDir, backupDir, name="test", keep=5)
 
 if __name__ == "__main__":
     main()
